e9625d7a15408ddc92ab3ca823fbf0bbbe7478b9
Stefan Thomas <justmoon@members.fsf.org>
2015-09-13 00:01:19 -0700

    [FEATURE] Support boolean  conditions.

--- src/lib/condition.js
+++ src/lib/condition.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const _ = require('lodash')
 const crypto = require('crypto')
 const tv4 = require('tv4')
+const formats = require('tv4-formats')
 
 const schemaJson = require('../../schemas/Condition.json')
 
+tv4.addFormat(formats)
 class Condition {
   static validate (condition) {
     return tv4.validateMultiple(condition, schemaJson)
@@ -13,23 +16,32 @@ class Condition {
   static testFulfillment (condition, fulfillment) {
     switch (condition.type) {
       case 'sha256':
-        return this.testSha256Fulfillment(condition, fulfillment)
+        return this.testHashSha256(condition, fulfillment)
       case 'before':
-        return this.testBefore(condition)
+        return this.testTimeBefore(condition)
+      case 'and':
+        return this.testBooleanAnd(condition, fulfillment)
       default:
         return false
     }
   }
 
-  static testSha256Fulfillment (condition, fulfillment) {
+  static testHashSha256 (condition, fulfillment) {
     const hash = crypto.createHash('sha256')
     hash.update(fulfillment.message)
     const digest = hash.digest('hex')
     return condition.digest === digest
   }
 
-  static testBefore (condition) {
+  static testTimeBefore (condition) {
     return Number(new Date()) < Number(new Date(condition.date))
+  }
+
+  static testBooleanAnd (condition, fulfillment) {
+    return _(condition.subconditions)
+      .zip(fulfillment.subfulfillments)
+      .map(_.spread(this.testFulfillment.bind(this)))
+      .every()
   }
 }
 
--- test/conditionSpec.js
+++ test/conditionSpec.js
@@ -41,6 +41,66 @@ describe('Condition', function () {
       expect(validationResult).to.be.an('object')
       expect(validationResult.valid).to.equal(true)
     })
+
+    it('should return valid: false for an invalid `before` condition', function () {
+      const condition = {
+        type: 'before',
+        date: '2015-09_13T05:43:23.508Z'
+      }
+
+      const validationResult = Condition.validate(condition)
+
+      expect(validationResult).to.be.an('object')
+      expect(validationResult.valid).to.equal(false)
+      expect(validationResult.errors).to.be.an('array')
+      expect(validationResult.errors).to.have.length.above(0)
+    })
+
+    it('should return valid: false for an invalid `before` condition #2', function () {
+      const condition = {
+        type: 'before',
+        date: '2015-09-33T05:43:23.508Z'
+      }
+
+      const validationResult = Condition.validate(condition)
+
+      expect(validationResult).to.be.an('object')
+      expect(validationResult.valid).to.equal(false)
+      expect(validationResult.errors).to.be.an('array')
+      expect(validationResult.errors).to.have.length.above(0)
+    })
+
+    it('should return valid: true for a valid `and` condition', function () {
+      const condition = {
+        type: 'and',
+        subconditions: [{
+          type: 'sha256',
+          digest: '50d858e0985ecc7f60418aaf0cc5ab587f42c2570a884095a9e8ccacd0f6545c'
+        }]
+      }
+
+      const validationResult = Condition.validate(condition)
+
+      expect(validationResult).to.be.an('object')
+      expect(validationResult.valid).to.equal(true)
+    })
+  })
+
+  it('should return valid: false for an `and` condition with an invalid subcondition', function () {
+    const condition = {
+      type: 'and',
+      subconditions: [{
+        type: 'sha256',
+        digest: '50d858e0985ecc7f60418aaf0cc5ab587f42c2570a884095a9e8ccacd0f65451c'
+      }]
+    }
+
+    const validationResult = Condition.validate(condition)
+
+    expect(validationResult).to.be.an('object')
+    expect(validationResult.valid).to.equal(false)
+    expect(validationResult.errors).to.be.an('array')
+    expect(validationResult.errors).to.have.length.above(0)
   })
 
   describe('testFulfillment', function () {
@@ -80,11 +140,130 @@ describe('Condition', function () {
         date: '2015-09-13T05:43:23.508Z'
       }
 
-      sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:20.000Z')))
+      const clock = sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:20.000Z')))
 
       const result = Condition.testFulfillment(condition)
 
       expect(result).to.equal(true)
+
+      clock.restore()
+    })
+
+    it('should return false for a `before` condition if the target date has been exactly reached', function () {
+      const condition = {
+        type: 'before',
+        date: '2015-09-13T05:43:23.508Z'
+      }
+
+      const clock = sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:23.508Z')))
+
+      const result = Condition.testFulfillment(condition)
+
+      expect(result).to.equal(false)
+
+      clock.restore()
+    })
+
+    it('should return false for a `before` condition if the target date is in the past', function () {
+      const condition = {
+        type: 'before',
+        date: '2015-09-13T05:43:23.508Z'
+      }
+
+      const clock = sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:24.000Z')))
+
+      const result = Condition.testFulfillment(condition)
+
+      expect(result).to.equal(false)
+
+      clock.restore()
+    })
+
+    it('should return true for an `and` condition with two valid subconditions', function () {
+      const condition = {
+        type: 'and',
+        subconditions: [{
+          type: 'before',
+          date: '2015-09-13T05:43:23.508Z'
+        }, {
+          type: 'sha256',
+          digest: '50d858e0985ecc7f60418aaf0cc5ab587f42c2570a884095a9e8ccacd0f6545c'
+        }]
+      }
+
+      const fulfillment = {
+        type: 'and',
+        subfulfillments: [{}, {
+          type: 'sha256',
+          message: 'example'
+        }]
+      }
+
+      const clock = sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:20.000Z')))
+
+      const result = Condition.testFulfillment(condition, fulfillment)
+
+      expect(result).to.equal(true)
+
+      clock.restore()
+    })
+
+    it('should return true for an `and` condition with one invalid subcondition', function () {
+      const condition = {
+        type: 'and',
+        subconditions: [{
+          type: 'before',
+          date: '2015-09-13T05:43:23.508Z'
+        }, {
+          type: 'sha256',
+          digest: '50d858e0985ecc7f60418aaf0cc5ab587f42c2570a884095a9e8ccacd0f6545c'
+        }]
+      }
+
+      const fulfillment = {
+        type: 'and',
+        subfulfillments: [{}, {
+          type: 'sha256',
+          message: 'example1'
+        }]
+      }
+
+      const clock = sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:20.000Z')))
+
+      const result = Condition.testFulfillment(condition, fulfillment)
+
+      expect(result).to.equal(false)
+
+      clock.restore()
+    })
+
+    it('should return true for an `and` condition with two invalid subconditions', function () {
+      const condition = {
+        type: 'and',
+        subconditions: [{
+          type: 'before',
+          date: '2015-09-13T05:43:13.508Z'
+        }, {
+          type: 'sha256',
+          digest: '50d858e0985ecc7f60418aaf0cc5ab587f42c2570a884095a9e8ccacd0f6545c'
+        }]
+      }
+
+      const fulfillment = {
+        type: 'and',
+        subfulfillments: [{}, {
+          type: 'sha256',
+          message: 'example1'
+        }]
+      }
+
+      const clock = sinon.useFakeTimers(Number(new Date('2015-09-13T05:43:20.000Z')))
+
+      const result = Condition.testFulfillment(condition, fulfillment)
+
+      expect(result).to.equal(false)
+
+      clock.restore()
     })
   })
 })
