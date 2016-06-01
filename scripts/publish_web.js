b33f5ec70cf67a044526c3cd2b044297578d0c72
Stefan Thomas <justmoon@users.noreply.github.com>
2016-05-31 17:15:16 -0700

    chore: auto-publish gh-pages (#35)

---
+++ scripts/publish_web.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const path = require('path')
+const exec = require('child_process').execSync
+
+let cwd = path.resolve(__dirname, '..')
+
+// Get current web branch
+console.log('\n# Cloning web branch')
+exec('rm -rf web', { cwd })
+exec('git clone git@github.com:interledger/five-bells-condition.git --branch gh-pages --single-branch web', { cwd })
+
+// Update jsdoc
+console.log('\n# Updating API docs')
+exec('npm run docs:jsdoc', { cwd })
+exec('mkdir -p web/jsdoc', { cwd })
+exec('cp -r jsdoc-out/* web/jsdoc/', { cwd })
+
+// Update spec
+cwd = path.resolve(cwd, 'web')
+exec('xml2rfc ../docs/spec.xml --html -o spec.html', { cwd })
+exec('xml2rfc ../docs/spec.xml --text -o spec.txt', { cwd })
+
+// Push changes
+console.log('\n# Pushing web branch')
+exec('cd web')
+exec('git add --all', { cwd })
+
+const status = exec('git status --porcelain', { cwd }).toString('utf8')
+if (!status.length) {
+  console.log('no changes')
+} else {
+  console.log(status)
+  exec('git commit -m \'chore: update gh-pages\'', { cwd })
+  exec('git push', { cwd })
+}
