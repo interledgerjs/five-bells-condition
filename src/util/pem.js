c8c087c8d15d9066e2ee6c0ca6dbba877ebb6278
Stefan Thomas <justmoon@members.fsf.org>
2016-12-26 07:49:08 -0800

    refactor: remove dependency on other asn1 package

--- src/util/pem.js
+++ src/util/pem.js
@@ -4,10 +4,81 @@
  * @module util
  */
 
-const ber = require('asn1').Ber
+const asn1 = require('asn1.js')
 
 // Crypto-conditions always use the same RSA exponent, namely 65537
 const RSA_EXPONENT = 65537
+
+/**
+ * ASN.1 schema for RSA public key.
+ *
+ * From RFC 3447, section A.1.1.
+ *
+ *    RSAPublicKey ::= SEQUENCE {
+ *      modulus           INTEGER,  -- n
+ *      publicExponent    INTEGER   -- e
+ *    }
+ *
+ * @type {asn1.Entity}
+ */
+const RsaPublicKey = asn1.define('RsaPublicKey', function () {
+  this.seq().obj(
+    this.key('modulus').int(),
+    this.key('publicExponent').int()
+  )
+})
+
+/**
+ * ASN.1 schema for RSA private key.
+ *
+ * From RFC 3447, section A.1.2.
+ *
+ *    RSAPrivateKey ::= SEQUENCE {
+ *      version           Version,
+ *      modulus           INTEGER,  -- n
+ *      publicExponent    INTEGER,  -- e
+ *      privateExponent   INTEGER,  -- d
+ *      prime1            INTEGER,  -- p
+ *      prime2            INTEGER,  -- q
+ *      exponent1         INTEGER,  -- d mod (p-1)
+ *      exponent2         INTEGER,  -- d mod (q-1)
+ *      coefficient       INTEGER,  -- (inverse of q) mod p
+ *      otherPrimeInfos   OtherPrimeInfos OPTIONAL
+ *    }
+ *
+ *    Version ::= INTEGER { two-prime(0), multi(1) }
+ *      (CONSTRAINED BY {
+ *        -- version must be multi if otherPrimeInfos present --
+ *      })
+ *
+ *    OtherPrimeInfos ::= SEQUENCE SIZE(1..MAX) OF OtherPrimeInfo
+ *
+ *    OtherPrimeInfo ::= SEQUENCE {
+ *      prime             INTEGER,  -- ri
+ *      exponent          INTEGER,  -- di
+ *      coefficient       INTEGER   -- ti
+ *    }
+ *
+ * @type {asn1.Entity}
+ */
+const RsaPrivateKey = asn1.define('RsaPrivateKey', function () {
+  this.seq().obj(
+    this.key('version').int(),
+    this.key('modulus').int(),
+    this.key('publicExponent').int(),
+    this.key('privateExponent').int(),
+    this.key('prime1').int(),
+    this.key('prime2').int(),
+    this.key('exponent1').int(),
+    this.key('exponent2').int(),
+    this.key('coefficient').int(),
+    this.optional().key('otherPrimeInfos').seqof(this.obj(
+      this.key('prime').int(),
+      this.key('exponent').int(),
+      this.key('coefficient').int()
+    ))
+  )
+})
 
 /**
  * Utilities for RSA-related DER/PEM encoding.
@@ -35,15 +106,14 @@ class Pem {
       modulus = Buffer.concat([new Buffer([0]), modulus])
     }
 
-    const berWriter = new ber.Writer()
-    berWriter.startSequence()
-    berWriter.writeBuffer(modulus, 2)
-    berWriter.writeInt(RSA_EXPONENT)
-    berWriter.endSequence()
+    const derPublicKey = RsaPublicKey.encode({
+      modulus,
+      publicExponent: RSA_EXPONENT
+    })
 
     return (
       '-----BEGIN RSA PUBLIC KEY-----\n' +
-      berWriter.buffer.toString('base64').match(/.{1,64}/g).join('\n') + '\n' +
+      derPublicKey.toString('base64').match(/.{1,64}/g).join('\n') + '\n' +
       '-----END RSA PUBLIC KEY-----\n'
     )
   }
@@ -61,11 +131,9 @@ class Pem {
       .replace(/\s+|\n\r|\n|\r$/gm, '')
     const buffer = new Buffer(pem, 'base64')
 
-    const reader = new ber.Reader(buffer)
-    reader.readSequence()
-    reader.readString(2, true) // version
-    const modulus = reader.readString(2, true) // modulus
-    return modulus[0] === 0 ? modulus.slice(1) : modulus
+    const decodedPrivateKey = RsaPrivateKey.decode(buffer)
+    const modulus = decodedPrivateKey.modulus.toBuffer()
+    return modulus
   }
 }
 
