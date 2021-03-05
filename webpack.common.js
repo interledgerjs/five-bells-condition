8d7cc234e7214ead96434c6e0111293a7bfc5b13
getlarge <ed@getlarge.eu>
2021-03-05 17:41:35 +0100

    fix: update lint config

--- webpack.common.js
+++ webpack.common.js
@@ -5,25 +5,25 @@
 const { paths } = require('./webpack.parts.js')
 
 module.exports = {
-    entry: paths.entry,
-    mode: 'none',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-        noEmitOnErrors: true
-    },
-    resolve: {
-        extensions: ['.js'],
-        modules: ['node_modules'],
-    },
+  entry: paths.entry,
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    noEmitOnErrors: true
+  },
+  resolve: {
+    extensions: ['.js'],
+    modules: ['node_modules']
+  }
 }
--- webpack.development.js
+++ webpack.development.js
@@ -5,26 +5,26 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    devtool: 'inline-source-map',
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                test: /vendor/,
-                sourceMap: false,
-            }),
-            new UglifyJsPlugin({
-                test: /^((?!(vendor)).)*.js$/,
-                sourceMap: true,
-            })
-        ],
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        },
-    },
+  devtool: 'inline-source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /vendor/,
+        sourceMap: false
+      }),
+      new UglifyJsPlugin({
+        test: /^((?!(vendor)).)*.js$/,
+        sourceMap: true
+      })
+    ],
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
 }
